fc2a643f5acd3b4e8211648395a0a08eb75ff811
benemoreirac <82347599+benemoreirac@users.noreply.github.com>
2021-04-11 21:23:06 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,10 @@
+tempoDeJogo = int(input("Quanto tempo temos já jogado? "))
+if tempoDeJogo <= 90:
+    print("Ainda tem jofo pela frente! Avanti Palestra!")
+    print("Avanti Palestra!")
+
+else:
+    print("Putz, ta acabando o jogo!")
+    print("Apita logo juiz!!")
+    
+    
