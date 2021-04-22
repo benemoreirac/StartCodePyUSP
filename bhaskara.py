37e20d27a9f3c7d9ef4d7bdbe92ea5ff03f532d8
benemoreirac <82347599+benemoreirac@users.noreply.github.com>
2021-04-22 11:21:03 -0300

    Add files via upload

--- bhaskara.py
+++ bhaskara.py
@@ -1,24 +1,32 @@
 import math
 
-a = int(input("Digite um valor para A: "))
-b = int(input("Digite um valor para B: "))
-c = int(input("Digite um valor para C: "))
+def delta(a, b, c):
+    return b**2-4*a*c
 
-delta = b**2-4*a*c
+def main():
+    a = int(input("Digite um valor para A: "))
+    b = int(input("Digite um valor para B: "))
+    c = int(input("Digite um valor para C: "))
+    imprime_raizes(a,b,c)
 
-
-if delta == 0:
-    x1 = ((-b)+ math.sqrt((b**2)-(4*a*c)))/(2*a)
-    print("A Unica raiz é: ", x1)
-
-else:
-    if delta < 0:
-        print("Esta equação não possui raizes!")
-    else:
+def imprime_raizes(a, b, c):
+    d = delta(a, b, c)
+    if d == 0:
         x1 = ((-b)+ math.sqrt((b**2)-(4*a*c)))/(2*a)
-        x2 = ((-b)- math.sqrt((b**2)-(4*a*c)))/(2*a)
-        print("A primeira raiz é: ",x1)
-        print("A segunda raiz é: ",x2)
+        print("A Unica raiz é: ", x1)
+    else:
+        if d < 0:
+            print("Esta equação não possui raizes!")
+        else:
+            x1 = ((-b)+ math.sqrt((b**2)-(4*a*c)))/(2*a)
+            x2 = ((-b)- math.sqrt((b**2)-(4*a*c)))/(2*a)
+            print("A primeira raiz é: ",x1)
+            print("A segunda raiz é: ",x2)
+
+
+
+
+
 
 
 
