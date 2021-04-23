95630293599fbb6347a4327f8e5278de8098a397
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2021-04-23 19:37:23 -0300

    Add files via upload

---
+++ cloneLista.py
@@ -0,0 +1,18 @@
+lista_principal = []
+lista_clone = []
+dado = 1
+
+
+def clone(lista):
+    for item in lista:
+        lista_clone.append(item)
+    return lista_clone        
+    
+
+while dado != 0:
+    dado = int(input("Digite um dado ou 0 para sair: "))
+    if dado != 0:
+        lista_principal.append(dado)
+
+print("Lista Principal: ", lista_principal)
+print("Lista Clone: ", clone(lista_principal))
