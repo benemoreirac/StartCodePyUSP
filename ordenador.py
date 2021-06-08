28abffbccbbc93e98df2c54f07014f006878fdc2
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2021-06-08 10:52:11 -0300

    Add files via upload

--- ordenador.py
+++ ordenador.py
@@ -21,3 +21,24 @@ class Ordenador:
             #coloca o menor elemento encontrado no inicio da sub-lista
             #para isso, troca de lugar os elementos nas posicoes i e posicao_do_minimo
             lista[i], lista[posicao_do_minimo] = lista[posicao_do_minimo], lista[i]
+
+    def bolha(self, lista):
+        fim = len(lista)
+
+        for i in range(fim-1, 0, -1):
+            for j in range(i):
+                if lista[j] >lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+
+    def bolha_curta (self, lista):
+        fim = len(lista)
+
+        for i in range(fim-1, 0, -1):
+            trocou = False
+            for j in range(i):
+                if lista[j] >lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+                    trocou = True
+
+            if not trocou:
+                return
